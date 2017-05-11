6e8cac474afa1e970462c2174bef950a06f4a78d
CarolinaTapia <cstapiarubio@gmail.com>
2017-05-11 13:58:28 -0300

    añadiendo mas diseño, falta poco

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,16 @@ document.getElementById("toogle").addListener("click", function event){
 	document.getElementById("nav-header").classList.toogle("open");
 	document.getElementById("body").classList.toogle("overflow-hidden");
 });
+
+
+var lastScrollTop=0;
+window.addEventListener("scroll" function(){
+	var currentScroll= window.pageYOffset || document.body.scrollTop;
+	if(currentScroll>lastScrollTop){
+		console.log("down");
+		else{
+			console.log("up");
+		}
+		lastScrollTop=currentScroll;
+	}
+}, false);
