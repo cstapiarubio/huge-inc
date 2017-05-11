7387ffb87dd576846d5cabf1702fd1360b070e3d
CarolinaTapia <cstapiarubio@gmail.com>
2017-05-11 12:34:12 -0300

    agregando hamburguesa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,5 @@
-main.js
+document.getElementById("toogle").addListener("click", function event){
+	event.preventDefault();
+	document.getElementById("nav-header").classList.toogle("open");
+	document.getElementById("body").classList.toogle("overflow-hidden");
+});
